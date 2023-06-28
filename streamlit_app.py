edbfa714c7f4f98aa17a10703d30ad1fa882c377
Matthew King <mking@resultant.com>
2023-06-28 13:30:33 -0400

    Update streamlit_app.py
    Started Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatment')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
